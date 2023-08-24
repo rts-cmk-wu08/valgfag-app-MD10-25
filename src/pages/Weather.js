295e1f44630aaae340e5b0aa0ff7ad2f0da1dbc4
MD.10-25 <martindimitrov04@gmail.com>
2023-08-24 15:15:48 +0200

    trying to get geo locationimplemeted

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,20 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 
 const Weather = () => {
-  const baseURL = "https://api.openweathermap.org/data/2.5/"; 
+  const baseURL = "https://api.openweathermap.org/data/2.5/";
+  const [myPos, setMyPos] = useState(null);
+  // const [lat, setLat] = useState();
+  // const [lon, setLon] = useState(); 
   const [search, setSearch] = useState("");
   const [weatherResults, setWeatherResults] = useState({});
+  const [posWeather, setPosWeather] = useState({});
 
   const searchPressed = () => {
     fetch(`${baseURL}weather?q=${search}&appid=${process.env.REACT_APP_OPENWEATHER}&units=metric`)
     .then(res => res.json())
     .then(result => {
-      console.log(result)
       setWeatherResults(result)
     })
   }
+
+  useEffect(() => {
+    if('geolocation' in navigator) {
+       navigator.geolocation.getCurrentPosition((position) => {
+          setMyPos(position)
+        })
+    }
+  }, []);
+
+  useEffect(() => {
+    if(myPos) {
+      fetch(`${baseURL}weather?lat=${myPos?.coords?.latitude}&lon=${myPos?.coords?.longitude}&appid=${process.env.REACT_APP_OPENWEATHER}&units=metric`)
+      .then(res => res.json())
+      .then(result => {
+        setPosWeather(result)
+      })
+    }
+  }, [myPos]);
+
+   
+
+
+
     return ( 
         <section>
             <h1>Check the weather</h1>
@@ -24,10 +50,10 @@ const Weather = () => {
                 onChange={(e) => setSearch(e.target.value)}
             />
             <button onClick={searchPressed}>Search</button>
-            <p>{weatherResults?.name}</p>
-            <p>{weatherResults?.main?.temp.toFixed(0)}</p>
-            <p>{weatherResults?.weather && weatherResults.weather[0]?.main}</p>
-            <p>{weatherResults?.weather && "("+weatherResults.weather[0]?.description+")"}</p>
+            <p>{search === "" ? posWeather?.name : weatherResults?.name}</p>
+            <p>{search === "" ? posWeather?.main?.temp.toFixed(0) : weatherResults?.main?.temp.toFixed(0)}</p>
+            <p>{search === "" ? posWeather?.weather && posWeather.weather[0]?.main : weatherResults?.weather && weatherResults.weather[0]?.main}</p>
+            <p>{search === "" ? posWeather?.weather && "("+posWeather.weather[0]?.description+")" : weatherResults?.weather && "("+weatherResults.weather[0]?.description+")"}</p>
         </section>
      );
 }
